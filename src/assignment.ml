c915e88e59279e7eadaf242a163f80241c7b4ac9
root <root@LAPTOP-TI289SBO.localdomain>
2021-12-10 19:37:50 +0100

    m

--- src/assignment.ml
+++ src/assignment.ml
@@ -1,1 +1,13 @@
-let interleave3 l1 l2 l3 = failwith "TODO interleave3"
+
+
+let rec inter2 l1 l2 acc =  
+  match l1 with 
+  | [] -> acc@l2
+  | x::xs -> inter2 l2 xs (acc@[x]);;
+
+let interleave3 l1 l2 l3 = 
+  let rec inter3 l1 l2 l3 acc = 
+  match l1 with 
+  | [] -> inter2 l2 l3 acc
+  | x::xs -> inter3 l2 l3 xs (acc@[x]) 
+  in inter3 l1 l2 l3 [];;
