2e93402fde2d71ef88d3a42933e1a95eaab953fb
root <root@LAPTOP-TI289SBO.localdomain>
2022-03-20 20:09:33 +0100

    m

--- src/assignment.ml
+++ src/assignment.ml
@@ -1,5 +1,10 @@
 type gameboard = char list list
 
+(* board, current_player, # of entities on the board *)
+type game_state = gameboard * bool * int
+type dec_tree = Node of game_state * dec_tree list * int
+(* the last int of dec_tree is the weight of the node. *)
+
 (*----------PRINT-FUNCTIONS---------- *)
 
 let to_string_row row =
@@ -10,6 +15,7 @@ let print_board brd = print_string (to_string_brd brd)
 (*----------------------------------- *)
 
 let init_board () = List.init 3 (fun _ -> List.init 3 (fun _ -> ' '))
+let init_state () = (init_board (), true, 0)
 
 let set j i x board =
   let ch = if x then 'X' else 'O' in
@@ -54,10 +60,6 @@ let has_won x brd =
   has_won_diagonal x brd || has_won_horizontal x brd || has_won_vertical x brd
 
 (*AI - LOGIC - BASICS [decision tree etc.]*)
-(* board, current_player, # of entities on the board *)
-type game_state = gameboard * bool * int
-type dec_tree = Node of game_state * dec_tree list * int
-(* the last int of dec_tree is the weight of the node. *)
 
 let make_move i j (board, x, num) =
   let board = set i j x board in
@@ -82,6 +84,11 @@ let build_tr (board, x, num) =
     helper 0 0 []
   in
   build_trr (board, x, num)
+
+let init_tree () =
+  let st = init_state () in
+  let trees = build_tr st in
+  Node (st, trees, 0)
 
 let rec search (Node (state, lst, _)) =
   match state with
@@ -108,11 +115,27 @@ let find_max = find_pred (fun a b -> a > b)
 
 (* the best path is the one where we win in the shortest amount of moves + we lose in the max. amount of moves *)
 
-let rec find_best_node lst =
-  List.fold_left
-    (fun (Node (_, _, weight) as curr) (Node (_, _, weight') as other) ->
-      if weight < weight' then curr else other)
-    (List.hd lst) (List.tl lst)
+(* LOGIC behind comparing different branches *)
+let check_tuple (old_max, old_min, _) (new_max, new_min, _) =
+  if old_max < -old_min then true
+  else if new_max < -new_min then false
+  else if new_max > old_max then true
+  else false
+
+let rec find_min_max (Node ((_, _, _), lst, _)) =
+  List.mapi (fun i node -> (find_max node, find_min node, i)) lst
+
+let find_best_node (Node ((_, _, _), lst, _) as node) =
+  let tuples = find_min_max node in
+  let ((min, max, index) as opt_tuple) =
+    List.fold_left
+      (fun acc ((m, mx, i) as v) -> if check_tuple acc v then v else acc)
+      (List.hd tuples) (List.tl tuples)
+  in
+  List.nth lst index
+
+let find_best_move node =
+  match find_best_node node with Node ((st, _, _), _, _) as node -> (node, st)
 
 let rec analyze (Node ((b, x, n), lst, weight)) =
   Node
@@ -122,11 +145,50 @@ let rec analyze (Node ((b, x, n), lst, weight)) =
       else if has_won false b then -(10 - n)
       else 0 )
 
-let rec find_min_max (Node ((_, _, _), lst, _)) =
-  List.map (fun node -> (find_max node, find_min node)) lst
+let build_tree () =
+  let tr = init_tree () in
+  analyze tr
 
-let check_tuple (old_max, old_min) (new_max, new_min) =
-  if -old_min < old_max then true
-  else if -new_min >= new_max then false
-  else if new_max < old_max then true
-  else false
+let build_tree_node node =
+  let node, board = find_best_move node in
+  match node with Node ((b, x, n), lst, _) as next_node -> next_node
+
+let rec lst_equals l1 l2 =
+  match (l1, l2) with
+  | [], [] -> true
+  | x :: xs, y :: ys -> x = y && lst_equals xs ys
+  | _, _ -> false
+
+(* Not one of the most efficient methods; can be improved! *)
+let rec board_equals b1 b2 =
+  match (b1, b2) with
+  | [], [] -> true
+  | x :: xs, y :: ys -> lst_equals x y && board_equals xs ys
+  | _, _ -> false
+
+let make_move i j (Node ((board, _, _), lst, _)) =
+  let board = set i j false board in
+  List.find (fun (Node ((board', _, _), _, _)) -> board_equals board board') lst
+
+let start_game () =
+  let rec run i node =
+    if i = 0 then print_string "draw!"
+    else
+      let (Node ((b, _, _), _, weight) as node) =
+        if i mod 2 = 0 then
+          let x =
+            print_string "x - coordinate:\n";
+            read_int ()
+          in
+          let y =
+            print_string "y - coordinate:\n";
+            read_int ()
+          in
+          make_move x y node
+        else build_tree_node node
+      in
+      print_board b;
+      print_string "\n\n";
+      if weight = 0 then run (i - 1) node else print_string "computer has won!"
+  in
+  run 9 (analyze (init_tree ()))
